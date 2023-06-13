3a1916a168011541facb48cf6c52c5e212b13c3b
TCHAKAL <ahmedtchakal@gmail.com>
2023-06-13 23:01:32 +0200

    Create UserRepository

---
+++ src/main/java/fr/tchakal/security/dao/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package fr.tchakal.security.dao.repository;
+
+import fr.tchakal.security.dao.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
